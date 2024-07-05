1e7d8ca05556a90b6144efd2cf00b508d4d530e6
Rodel Arenas <rodelarenas@gmail.com>
2024-07-05 10:59:52 +0800

    Update intellinews.py

--- intellinews.py
+++ intellinews.py
@@ -1,6 +1,7 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import subprocess
 subprocess.run([f"{sys.executable}", "-m","spacy","download","en_core_web_sm", "--user"])
 
 
